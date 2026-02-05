d73bcbd2773a67c2bf56088184cdd046c8efdd15
Jerry Wang <jwang414@students.cs.ubc.ca>
2026-02-05 13:43:42 -0800

    Updated Notebook class specification, Added Tests for Notebook class

--- src/main/model/NoteBook.java
+++ src/main/model/NoteBook.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.List;
 
+// Represents and operates on a collection of Notes 
 public class NoteBook {
     
     // EFFECTS: Creates an empty notebook with an empty list of notes
@@ -10,18 +11,21 @@ public class NoteBook {
     }
 
     // MODIFIES: this
-    // EFFECTS: adds note to the list of notes
-    public void addNote(Note n) {
-        // stub
+    // EFFECTS: if note is not yet in list of notes, adds note to the list of notes and return true
+    // else, do nothing and return false
+    public boolean addNote(Note n) {
+        return false; // stub
     }
 
     // MODIFIES: this
-    // EFFECTS: removes note from the list of notes
-    public void deleteNote(Note n) {
-        // stub
+    // EFFECTS: if note is found in list of notes, removes note from the list of notes and returns true
+    // else, do nothing and return false
+    public boolean deleteNote(Note n) {
+        return false; // stub
     }
 
-    public Note getNote() {
+    // EFFECTS: returns note found with given title in list of notes, else null
+    public Note getNote(String title) {
         return null; // stub
     }
 
---
+++ src/test/model/TestNoteBook.java
@@ -0,0 +1,95 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestNoteBook {
+    
+    private NoteBook notebook;
+    private ArrayList<Note> noteslist;
+    private Note note1;
+    private Note note2;
+
+    @BeforeEach
+    void setup() {
+        notebook = new NoteBook();
+        noteslist = new ArrayList<Note>();
+        note1 = new Note("note1");
+        note2 = new Note("note2");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals(noteslist, notebook.getAllNotes());
+    }
+
+    @Test
+    void testAddNoteOnce() {
+        assertTrue(notebook.addNote(note1));
+        noteslist.add(note1);
+        assertEquals(noteslist, notebook.getAllNotes());
+    } 
+
+    @Test
+    void testAddNoteMultipleNoRepeat() {
+        assertTrue(notebook.addNote(note1));
+        assertTrue(notebook.addNote(note2));
+        noteslist.add(note1);
+        noteslist.add(note2);
+        assertEquals(noteslist, notebook.getAllNotes());
+    }
+
+    @Test
+    void testAddNoteMultipleRepeat() {
+        assertTrue(notebook.addNote(note1));
+        assertFalse(notebook.addNote(note1));
+        noteslist.add(note1);
+        assertEquals(noteslist, notebook.getAllNotes());
+    }
+
+    @Test
+    void testDeleteNoteOnce() {
+        notebook.addNote(note1);
+        assertTrue(notebook.deleteNote(note1));
+        assertEquals(noteslist, notebook.getAllNotes());
+    }
+
+    @Test
+    void testDeleteNoteEmpty() {
+        assertFalse(notebook.deleteNote(note1));
+        assertEquals(noteslist, notebook.getAllNotes());   
+    }
+
+    @Test
+    void deleteNoteMultiple() {
+        notebook.addNote(note1);
+        notebook.addNote(note2);
+        assertTrue(notebook.deleteNote(note1));
+        assertTrue(notebook.deleteNote(note2));
+        assertEquals(noteslist, notebook.getAllNotes());
+    }
+
+    @Test
+    void getNote() {
+        assertNull(notebook.getNote("note1"));
+        notebook.addNote(note1);
+        assertEquals(note1, notebook.getNote("note1"));
+    }
+
+    @Test void getAllNotes() {
+        assertEquals(noteslist, notebook.getAllNotes());
+        notebook.addNote(note1);
+        noteslist.add(note1);
+        assertEquals(noteslist, notebook.getAllNotes());
+        notebook.addNote(note2);
+        noteslist.add(note2);
+        assertEquals(noteslist, notebook.getAllNotes());
+    }
+}
