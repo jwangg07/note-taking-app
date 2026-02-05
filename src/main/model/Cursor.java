60e5511f335ce7a14b6954c0a1a0d013cee97538
Jerry Wang <jwang414@students.cs.ubc.ca>
2026-02-05 15:15:27 -0800

    Added Implementation for Cursor class

--- src/main/model/Cursor.java
+++ src/main/model/Cursor.java
@@ -2,46 +2,52 @@ package model;
 
 public class Cursor {
 
+    private int row;
+    private int col;
+
     // EFFECTS: Creates a cursor at row 0 and column 0
     public Cursor() {
-        // stub
+        row = 0;
+        col = 0;
     }
 
+    // REQUIRES: row > 0, col > 0
     // EFFECTS: Creates a cursor at given row and column (FOR TEST PURPOSES)
     public Cursor(int row, int col) {
-        // stub
+        this.row = row;
+        this.col = col;
     }
 
     // MODIFIES: this
     // EFFECTS: moves the cursor up a row
     public void moveUp() {
-        
+        if (row > 0) row--;
     }
 
     // MODIFIES: this
     // EFFECTS: moves the cursor to a column to the right
     public void moveRight() {
-
+        col++;
     }
 
     // MODIFIES: this
     // EFFECTS: moves the cursow down a row
     public void moveDown() {
-
+        row++;
     }
 
     // MODIFIES: this
     // EFFECTS: moves the cursor to a column to the left
     public void moveLeft() {
-
+        if (col > 0) col--;
     }
 
     public int getRow() {
-        return 0; // stub
+        return row;
     }
 
     public int getColumn() {
-        return 0; // stub
+        return col;
     }
 
     // TODO: set a char limit, moving right at char limit goes to next row
