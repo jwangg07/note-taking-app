ed5d646a866eaf82afbe13c78e98aed9b0618ab8
Jerry Wang <jwang414@students.cs.ubc.ca>
2026-02-05 14:54:22 -0800

    Added specification for Cursor class

---
+++ src/main/model/Cursor.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Cursor {
+
+    // EFFECTS: Creates a cursor at row 0 and column 0
+    public Cursor() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: moves the cursor up a row
+    public void moveUp() {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: moves the cursor to a column to the right
+    public void moveRight() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: moves the cursow down a row
+    public void moveDown() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: moves the cursor to a column to the left
+    public void moveLeft() {
+
+    }
+
+    public int getCursorRow() {
+        return 0; // stub
+    }
+
+    public int getCursorColumn() {
+        return 0; // stub
+    }
+}
