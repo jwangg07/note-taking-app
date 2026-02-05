c4354495aadbed86ff6f7badeba3d6fd54e5006f
Jerry Wang <jwang414@students.cs.ubc.ca>
2026-02-05 11:49:33 -0800

    Created Specification for Note class

---
+++ src/main/model/Note.java
@@ -0,0 +1,26 @@
+package model;
+
+
+public class Note {
+
+    // EFFECTS: Creates an empty note with given title and no content
+    public Note(String title) {
+        // stub
+    }
+
+    public String getTitle() {
+        return null; // stub
+    }
+
+    public void setTitle(String title) {
+        // stub
+    }
+
+    public String getContent() {
+        return null; // stub
+    }
+
+    public void setContent(String content) {
+        // stub
+    }
+}
