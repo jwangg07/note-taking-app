87293aa62900a71f18e8ecf99f36bff8131a698e
Jerry Wang <jwang414@students.cs.ubc.ca>
2026-02-05 10:10:21 -0800

    Added Specification for NoteBook class

---
+++ src/main/model/NoteBook.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.List;
+
+public class NoteBook {
+    
+    // EFFECTS: Creates an empty notebook with an empty list of notes
+    public NoteBook() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds note to the list of notes
+    public void addNote(Note n) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes note from the list of notes
+    public void deleteNote(Note n) {
+        // stub
+    }
+
+    public Note getNote() {
+        return null; // stub
+    }
+
+    public List<Note> getAllNotes() {
+        return null; // stub
+    }
+}
