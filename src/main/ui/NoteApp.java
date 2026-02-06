7de038117d6eb70013e8f2e9c8c7ba1c0df00fce
Jerry Wang <jwang414@students.cs.ubc.ca>
2026-02-06 11:56:22 -0800

    Added Specification to NoteApp class

--- src/main/ui/NoteApp.java
+++ src/main/ui/NoteApp.java
@@ -2,7 +2,53 @@ package ui;
 
 public class NoteApp {
 
+    // EFFECTS: creates notes app with new notebook
     public NoteApp() {
+
+    }
+
+    // EFFECTS: run application until user quits
+    public void run() {
+        // stub
+    }
+
+    // EFFECTS: prints initial instructions to use notebook
+    public void printInstructions() {
+        // stub
+    }
+
+    // EFFECTS: displays a list of notes in notebook by title
+    public void displayNotes() {
+
+    }
+
+    // EFFECTS: prompts user for title and content of the note and adds note to notebook
+    public void createNote() {
+
+    }
+
+    // EFFECTS: prompts user for confirmation and deletes note from notebook
+    public void deleteNote() {
+
+    }
+
+    // EFFECTS: displays title and content of a note, prompts user if they want to edit
+    public void selectNote() {
+
+    }
+
+    // EFFECTS: sets title of selected note to user input
+    public void editNoteTitle() {
+
+    }
+
+    // EFFECTS: sets content of selected note to user input
+    public void editNoteContent() {
+
+    }
+
+    // EFFECTS: ends the program
+    public void end() {
 
     }
 }
