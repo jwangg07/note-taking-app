344010bbe51ee731df5112f88db308c0d42a153b
Jerry Wang <jwang414@students.cs.ubc.ca>
2026-02-06 13:07:12 -0800

    Updated Specification for InputHandler class

--- src/main/ui/InputHandler.java
+++ src/main/ui/InputHandler.java
@@ -1,7 +1,15 @@
 package ui;
 
 public class InputHandler {
-    public InputHandler() {
-        
+
+    // EFFECTS: asks user for input with a message and returns user input
+    public String promptInput(String message) {
+        return ""; // stub
     }
+
+    // EFFECTS: returns user input
+    private String getUserInput() {
+        return ""; // stub
+    }
+
 }
