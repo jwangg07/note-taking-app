ee75de6cf19efbc0c5cd4eb552cade50e2bf6967
Jerry Wang <jwang414@students.cs.ubc.ca>
2026-02-06 19:14:01 -0800

    Updated Implementation for Main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,6 +3,7 @@ package ui;
 public class Main {
     public static void main(String[] args) throws Exception {
         NoteApp app = new NoteApp();
-        InputHandler input = new InputHandler();
+        app.printInstructions();
+        app.run();
     }
 }
