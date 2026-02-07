3e982ea27631cbcf2966523951834dda78346bc8
Jerry Wang <jwang414@students.cs.ubc.ca>
2026-02-06 18:45:04 -0800

    Updated Specification for InputHandler class

--- src/main/ui/InputHandler.java
+++ src/main/ui/InputHandler.java
@@ -2,10 +2,12 @@ package ui;
 
 import java.util.Scanner;
 
+// Handles user input
 public class InputHandler {
 
     private Scanner scanner;
 
+    // EFFECTS: creates InputHandler with scanner
     public InputHandler() {
         scanner = new Scanner(System.in);
     }
@@ -21,4 +23,38 @@ public class InputHandler {
         return scanner.nextLine();
     }
 
+    public void parseInputMain(String input, NoteApp app) {
+        switch (input) {
+            case "a": // add note
+                // stub
+                break;
+            case "s": // select note
+                // stub
+                break;
+            case "q": // quit
+                // stub
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void parseInputNote(String input, NoteApp app) {
+        switch (input) {
+            case "t": // edit title
+                // stub
+                break;
+            case "c": // edit content
+                // stub
+                break;
+            case "d": // delete note
+                // stub
+                break;
+            case "b": // back
+                // stub
+                break;
+            default:
+                break;
+        }
+    }
 }
