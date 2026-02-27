1656b5dbd0cec258cfbba5315e57fdf1d9d3f8ef
Jerry Wang <jwang414@students.cs.ubc.ca>
2026-02-26 21:43:18 -0800

    Added specification for WriteJson

---
+++ src/main/persistance/WriteJson.java
@@ -0,0 +1,21 @@
+package persistance;
+
+import java.io.FileNotFoundException;
+
+import model.NoteBook;
+
+public class WriteJson {
+    
+    // EFFECTS: Constructs a writer to write a JSON file to a given path
+    public WriteJson(String path) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Converts a notebook to a JSON file and saves it locally
+    // throws FileNotFoundException if the path file does not exist 
+    public void write(NoteBook notebook) throws FileNotFoundException {
+        
+    }
+
+}
